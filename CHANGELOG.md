23ff1ac1af66757e4ee33adc0a1cc30d240b2a94
Benjamin Rancourt <benjamin.rancourt@usherbrooke.ca>
2019-10-14 18:19:20 -0400

    [3.0.1] Chores (#5)
    - Update dependencies.
    - Add package-lock.json to gitignore.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+## [3.0.1] - 2019-10-14
+### Chores
+- Update dependencies.
+- Add package-lock.json to gitignore.
 
 ## [3.0.0] - 2019-07-07
 ### Added
--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,14 @@
 /* eslint-env node */
-const createDefaultConfig = require('@open-wc/testing-karma/default-config');
-const merge = require('webpack-merge');
+const { createDefaultConfig } = require('@open-wc/testing-karma');
+const merge = require('deepmerge');
 
 module.exports = (config) => {
   config.set(
     merge(createDefaultConfig(config), {
+      esm: {
+        nodeResolve: true,
+      },
+
       files: [{
         pattern: 'test/**/*.test.js',
         type: 'module',
