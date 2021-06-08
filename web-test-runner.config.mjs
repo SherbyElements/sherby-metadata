45c4b6760dcdac681f868feffca9a983a52528c9
Benjamin Rancourt <benjamin.rancourt@usherbrooke.ca>
2021-06-07 20:27:31 -0400

    Test Chrome

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,16 +1,7 @@
-import {chromeLauncher} from '@web/test-runner-chrome';
-
 export default {
-  nodeResolve: true,
   coverage: true,
   files: [
     "test/**/*.test.js"
   ],
-  browsers: [
-    chromeLauncher({
-      launchOptions: {
-        executablePath: '/usr/bin/google-chrome'
-      },
-    }),
-  ],
+  nodeResolve: true,
 };
